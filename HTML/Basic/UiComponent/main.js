a9710844da36eeb14ff89e1c762ddcb3263659f7
phanivelli <vdurgaphanikumar@gmail.com>
2019-09-19 14:36:33 +0530

    my gate page

---
+++ HTML/Basic/UiComponent/main.js
@@ -0,0 +1,8 @@
+function showPop() {
+  document.getElementById("showModal").style.visibility = "visible";
+  document.getElementById("showDialouge").style.visibility = "visible";
+}
+function hidePop() {
+  document.getElementById("showModal").style.visibility = "hidden";
+  document.getElementById("showDialouge").style.visibility = "hidden";
+}
